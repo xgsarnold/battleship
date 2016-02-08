c833af14f42b315133449c182f9c65b3ef001cb3
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-02-08 18:49:46 -0500

    Pass test 12

--- battleship.rb
+++ battleship.rb
@@ -34,10 +34,15 @@ class Ship
   def initialize(length)
     @length = length
     @positions = []
+    @hit_positions = []
   end
 
   def length
     @length
+  end
+
+  def positions
+    @positions
   end
 
   def place(x, y, across)
@@ -57,5 +62,24 @@ class Ship
 
   def covers?(x, y)
     @positions.include?([x, y])
+  end
+
+  def overlaps_with?(other_ship)
+    @positions.each do |place|
+      return true if other_ship.covers?(place[0], place[1])
+    end
+    false
+  end
+
+  def fire_at(x, y)
+    if @hit_positions.empty?
+      if @positions.include?([x, y])
+        @hit_positions << [x, y]
+      else
+        false
+      end
+    else
+      false
+    end
   end
 end
